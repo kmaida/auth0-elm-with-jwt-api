308b0683d3d7d3901b87d01f1b35739962ae34f4
Kim Maida <git@kim-maida.com>
2016-08-09 22:48:21 -0400

    STEP 4: add login and logout functionality.

--- src/Main.elm
+++ src/Main.elm
@@ -61,7 +61,11 @@ randomQuoteUrl =
     
 registerUrl : String
 registerUrl =
-    api ++ "users"       
+    api ++ "users"  
+    
+loginUrl : String
+loginUrl =
+    api ++ "sessions/create"       
 
 -- GET a random quote (unauthenticated)
     
@@ -79,7 +83,7 @@ userEncoder : Model -> Encode.Value
 userEncoder model = 
     Encode.object 
         [ ("username", Encode.string model.username)
-        , ("password", Encode.string model.password) 
+        , ("password", Encode.string model.password)
         ]          
 
 -- POST register / login request
@@ -114,7 +118,9 @@ type Msg
     | SetUsername String
     | SetPassword String
     | ClickRegisterUser
+    | ClickLogIn
     | GetTokenSuccess String
+    | LogOut
 
 -- Update
 
@@ -142,14 +148,21 @@ update msg model =
         ClickRegisterUser ->
             ( model, authUserCmd model registerUrl )
 
+        ClickLogIn ->
+            ( model, authUserCmd model loginUrl ) 
+
         GetTokenSuccess newToken ->
             ( { model | token = newToken, password = "", errorMsg = "" } |> Debug.log "got new token", Cmd.none )  
+            
+        LogOut ->
+            ( { model | username = "", token = "" }, Cmd.none )
                        
 {-
     VIEW
     * Hide sections of view depending on authenticaton state of model
     * Get a quote
-    * Register
+    * Log In or Register
+    * Get a protected quote
 -}
 
 view : Model -> Html Msg
@@ -158,7 +171,7 @@ view model =
         -- Is the user logged in?
         loggedIn : Bool
         loggedIn =
-            if String.length model.token > 0 then True else False 
+            if String.length model.token > 0 then True else False
 
         -- If the user is logged in, show a greeting; if logged out, show the login/register form
         authBoxView =
@@ -171,13 +184,16 @@ view model =
                 -- Greet a logged in user by username
                 greeting : String
                 greeting =
-                    "Hello, " ++ model.username ++ "!"
+                    "Hello, " ++ model.username ++ "!" 
 
-            in        
+            in
                 if loggedIn then
                     div [id "greeting" ][
                         h3 [ class "text-center" ] [ text greeting ]
-                        , p [ class "text-center" ] [ text "You have super-secret access to protected quotes." ]  
+                        , p [ class "text-center" ] [ text "You have super-secret access to protected quotes." ]
+                        , p [ class "text-center" ] [
+                            button [ class "btn btn-danger", onClick LogOut ] [ text "Log Out" ]
+                        ]   
                     ] 
                 else
                     div [ id "form" ] [
@@ -199,7 +215,8 @@ view model =
                             ]    
                         ]
                         , div [ class "text-center" ] [
-                            button [ class "btn btn-link", onClick ClickRegisterUser ] [ text "Register" ]
+                            button [ class "btn btn-primary", onClick ClickLogIn ] [ text "Log In" ]
+                            , button [ class "btn btn-link", onClick ClickRegisterUser ] [ text "Register" ]
                         ] 
                     ]
                            
